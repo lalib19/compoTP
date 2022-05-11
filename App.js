28e5bbac87eb135e2b1513e280d853f39882b99a
louis alibert <louis.alib19@gmail.com>
2022-05-11 16:21:12 +0200

    components tp

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import json from "./tp.json"
+
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+    <ScrollView style={styles.scroll}>
+
+      {json.map((data) => 
+      <View style={styles.cards}>
+        <Image style={{width: '30%', height: '100%'}} source={{uri: data.img}}/>
+        <View style={styles.textCards}>
+        <Text style={styles.title}>{data.title}</Text>
+        <Text>{data.desc}</Text>
+      </View>
+      </View>
+      )}
+
+    </ScrollView>
     </View>
   );
 }
@@ -13,8 +29,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff9f0',    
   },
+  scroll: {
+    flex: 1,
+  },
+  cards: {
+    flex: 1,
+    flexDirection: "row",
+    margin: 15,
+    backgroundColor: "#f5e3d3",
+    borderRadius: 10,
+    padding: 10
+  },
+  textCards: {
+    flex:1,
+    marginLeft: 10,
+  },
+  title: {
+    fontSize: 20,
+    textDecorationLine: "underline",
+    marginBottom: 5,
+  }
+
 });
